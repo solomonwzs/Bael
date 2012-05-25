9989ecb55d4bcf1b09dd4cd5d99bf6afae73f0c1
Solomon Wu <solomon.wzs@gmail.com>
2012-05-25 18:37:25 +0800

    modified:   priv/www/resoure/css/global.css 	new file:   priv/www/resoure/js/bael_nav.js

---
+++ priv/www/resoure/js/bael_nav.js
@@ -0,0 +1,37 @@
+$(function(){
+	$('.bael_nav0').each(function(){
+		var li=$(this).find('li');
+		var len=li.size();
+		for (var i=0; i<len; i++){
+			var obj=$(li[i]);
+			if (i===0){
+				obj.addClass('selected');
+			}
+			obj.css('width', Math.floor($(this).width()/len)-22);
+		}
+	});
+
+	$('.bael_nav1').each(function(){
+		var li=$(this).find('li');
+		var len=li.size();
+		for (var i=0; i<len; i++){
+			var obj=$(li[i]);
+			if (i===0){
+				obj.css('border-left','none').addClass('selected');
+			}
+			obj.css('width', Math.floor($(this).width()/len)-1);
+		}
+	});
+
+	$('.bael_nav2').each(function(){
+		var li=$(this).find('li');
+		var len=li.size();
+		for (var i=0; i<len; i++){
+			var obj=$(li[i]);
+			if (i===0){
+				obj.addClass('selected');
+			}
+			obj.css('width', Math.floor($(this).width()/len)-1);
+		}
+	});
+});
